3a872867d89587301e3b6dbc8d1081b5f73e160a
eddie <eddie@5xruby.tw>
2021-08-30 15:05:04 +0800

    add more test

--- bank_account.js
+++ bank_account.js
@@ -1,14 +1,29 @@
 class BankAccount {
-  constructor(m) {
-    this.money = m
+  constructor(initMoney) {
+    this.money = initMoney
   }
 
   deposit(money) {
-    this.money += money
+    if (money > 0) {
+      this.money += money
+    }
   }
 
-  balance() {
+  withdraw(money) {
+    if (money > 0 && this.enough(money)) {
+      this.money -= money
+      return money
+    }
+
+    return 0
+  }
+
+  get balance() {
     return this.money
+  }
+
+  enough(money) {
+    return this.money >= money
   }
 }
 
--- bank_account.test.js
+++ bank_account.test.js
@@ -1,22 +1,46 @@
 const BankAccount = require("./bank_account")
 
+// BDD - Behavior-Driven Development
 describe("存錢功能", () => {
   it("可以存錢", () => {
-    // 3A 原則
-    // Arrange
     let bankAccount = new BankAccount(10)
-
-    // Act
     bankAccount.deposit(5)
-
-    // Assert
-    expect(bankAccount.balance()).toBe(15)
+    expect(bankAccount.balance).toBe(15)
   })
-  // 2. 不可以存 0 元或是小於 0 元的金額（越存錢越少！）
+
+  it("不可以存 0 元或是小於 0 元的金額", () => {
+    let bankAccount = new BankAccount(10)
+    bankAccount.deposit(-5)
+    expect(bankAccount.balance).toBe(10)
+  })
 })
 
 describe("領錢功能", () => {
-  // 3. 可以領錢
-  // 4. 不能領 0 元或是小於 0 元的金額（越領錢越多！）
-  // 5. 不能領超過本身餘額
+  it("可以領錢", () => {
+    // test case
+    let bankAccount = new BankAccount(10)
+
+    let amount = bankAccount.withdraw(6)
+
+    expect(bankAccount.balance).toBe(4)
+    expect(amount).toBe(6)
+  })
+
+  it("不能領 0 元或是小於 0 元的金額", () => {
+    let bankAccount = new BankAccount(10)
+
+    let amount = bankAccount.withdraw(-5)
+
+    expect(bankAccount.balance).toBe(10)
+    expect(amount).toBe(0)
+  })
+
+  it("不能領超過本身餘額", () => {
+    let bankAccount = new BankAccount(10)
+
+    let amount = bankAccount.withdraw(20)
+
+    expect(bankAccount.balance).toBe(10)
+    expect(amount).toBe(0)
+  })
 })
